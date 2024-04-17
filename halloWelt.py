4bfb89a86c50b7e6d5e7589774a6f8179d095f3f
AndySchw <166611836+QuereinsteigerIT@users.noreply.github.com>
2024-04-17 20:10:15 +0200

    Python Grundlagen

---
+++ halloWelt.py
@@ -0,0 +1,53 @@
+
+# Das ist mein erstes Programm vom 10.04.2024 2. LiveStream
+print("alles andere")
+
+
+# Variablen werden immer überschrieben und die letzte vor der Ausgabe wird genutzt
+daten = "Hallo Welt"
+daten = "Hallo Erde"
+
+#print(daten)
+
+
+# Die Variable wird immer überschrieben
+daten = "Hallo Welt"
+print(daten)
+daten = "Hallo Erde"
+print(daten)
+
+
+# Datentypen
+string = "string"
+integer = 1234
+float = 1234,234
+list = [0,1,2,3,4,5,6]
+tuple = (0,1,2,3,4,5,6)
+bool = True
+bool = False
+dict = {"name": "Karl", "alter": 26}
+
+#Opperatroren 
+
+plus = "+"
+print(5+2)
+minus = "-"
+print(5-2)
+multiplikation = "*"
+print(5*2)
+exponentiation = "**" #5**3=125
+print(5**3)
+division = "/"
+print(6/3)
+ganzzahligeDivision = "//" # 5//2 = 2
+print(5//2)
+groesserals = ">"
+print(5>2)
+kleinerals = "<"
+print(5<2)
+modulus = "%"# 5/2 = 1
+print(5%2)
+istgleich = "==" # 1 == 1 True, "Hallo" == "hallo"
+print(1==1)
+ungleich = "!=" # 1 =! 0 True
+print(1!=1)
